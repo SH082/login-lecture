80893c53aa7170f1296dbb24577d48502197f12c
김송훈 <ksh@gimsonghun-ui-MacBookAir.local>
2023-07-11 21:27:45 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,38 +1,42 @@
 "use strict"
-
-class UserStorage {
-    static #users = {
-        id: ["woorimIT", "나개발", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name:["우리밋", "나개발", "김팀장"],
-    };
-
-   static getUsers(...fields){
-    const users =this.#users;
-    const newUsers=fields.reduce((newUsers,field)=>{
-        if(users.hasOwnProperty(field)){
-            newUsers[field]=users[field];
-        }
-        return newUsers;
-    },{});
-        return newUsers;
-    }
-    static getUserInfo(id){
-        const users = this.#users;
+const fs = require("fs").promises;
+class UserStorage {/*암묵적으로 private한 변수나 메서드는 코드 최상단에 위치*/ 
+    static #getUserInfo(data,id){
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users)
-        const userInfo = usersKeys.reduce((newUser, info)=>{
-            newUser[info]=users[info][idx];
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
             return newUser;
         }, {});
-    return userInfo;
+
+        return userInfo;
     }
+    static getUsers(...fields) {
+        //const users =this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+    static getUserInfo(id) {
+        return fs
+            .readFile("./src/databases/users.json")
+            .then(data => {
+               return this.#getUserInfo();
+            })
+            .catch(console.error);
+    }
+    
     static save(userInfo) {
-        const users=this.#users;
+        // const users=this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return {success: true };
+        return { success: true };
     }
 }
-module.exports=UserStorage;
+module.exports = UserStorage;
