8d614cfcf54ec78192778c3b9df11b143125163d
김송훈 <ksh@gimsonghun-ui-MacBookAir.local>
2023-07-13 17:01:50 +0900

    AWS RDS 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 "use district";
 
 // 모듈
-const express =require("express");
-const bodyParser =require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 const app= express();
 
 // 라우팅
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,59 +1,29 @@
 "use strict"
-const fs = require("fs").promises;
-class UserStorage {/*암묵적으로 private한 변수나 메서드는 코드 최상단에 위치*/ 
-    static #getUserInfo(data,id){
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
+const db = require("../config/db");
 
-        return userInfo;
-    }
+class UserStorage {
     
-    static #getUsers(data, isAll, fields){
-        const users =JSON.parse(data); 
-        if(isAll) return users;
-
-        const newUsers = fields.reduce((newUsers, field) => {   
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-
-    static getUsers(isAll, ...fields) {
-        return fs
-            .readFile("./src/databases/users.json")
-            .then(data => {
-               return this.#getUsers(data, isAll, fields);
-            })
-            .catch(console.error);
-    }
-
     static getUserInfo(id) {
-        return fs
-            .readFile("./src/databases/users.json")
-            .then(data => {
-               return this.#getUserInfo(data,id);
-            })
-            .catch(console.error);
+        return new Promise((resolve, reject)=>{
+            const query = "SELECT * FROM users WHERE id = ?;";
+            db.query(query, [id], (err,data)=>{
+                if (err) reject(`${err}`);
+                resolve(data[0]);
+             });
+        });
+    
     }
     
     static async save(userInfo) {
-        const users = await this.getUsers(true);
-        if(users.id.includes(userInfo.id)){
-            throw ("이미 존재하는 아이디입니다.");
-        }    
-        users.id.push(userInfo.id);
-        users.psword.push(userInfo.psword);
-        users.name.push(userInfo.name);
-        fs.writeFile("./src/databases/users.json",JSON.stringify(users));
-        return {success:true};
+        return new Promise((resolve, reject)=>{
+            const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
+            db.query(query, 
+                [userInfo.id, userInfo.name, userInfo.psword], 
+                (err)=>{
+                if (err) reject(`${err}`);
+                resolve({success: true});
+             });
+        });
     }
 }
 module.exports = UserStorage;
